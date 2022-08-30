1a2dac83e99ed1207e0a3321257273d23b059dfa
iamFawadulHassan <112376159+iamFawadulHassan@users.noreply.github.com>
2022-08-30 16:26:15 +0100

    Create ifelse.cpp

---
+++ ifelse.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int age;
+	cout<<"Enter your age ";
+	cin>>age;
+	
+	if(age>=18)
+	{
+		cout<<"Yes, you can caste the vote";
+	}	
+	else
+	{
+		cout<<"No, you can't caste the vote";
+	}
+}
