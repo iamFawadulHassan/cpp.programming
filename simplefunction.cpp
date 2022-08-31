5c801ec6849c76873ab9d29e46d251b1ac95b904
iamFawadulHassan <112376159+iamFawadulHassan@users.noreply.github.com>
2022-08-31 20:01:20 +0100

    Create simplefunction.cpp

---
+++ simplefunction.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+void line(); //declaration
+int main()
+{
+	cout<<"Hello"<<endl;
+	line();
+	cout<<"I am Fawad ul Hassan"<<endl;
+	line();
+	cout<<"I am a student"<<endl;
+	line ();	
+}
+
+void line()
+{
+	cout<<"================="<<endl;
+}
