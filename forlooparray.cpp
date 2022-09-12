94dd068e33356ca2c843d838e731434b7fac360a
iamFawadulHassan <112376159+iamFawadulHassan@users.noreply.github.com>
2022-09-12 16:45:28 +0100

    Create forlooparray.cpp

---
+++ forlooparray.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{ 
+	int Size=5;
+	int a[Size];
+	for(int i=0; i<Size; i++)
+	{
+		cin>>a[i];
+	}
+	for(int j=0; j<Size; j++)
+	{
+		cout<<a[j]<<endl;
+	}
+}
